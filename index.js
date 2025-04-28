6d435fd5dbd2b20e5f27edf0feb258b52ee8ba4d
DesisEviL <aem3.asp@gmail.com>
2025-04-28 12:27:08 +0300

    2nd test

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ app.use(unknownEndpoint)
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT} for testing`)
 ;
     
 })
